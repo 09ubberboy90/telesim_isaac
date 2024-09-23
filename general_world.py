a1c42481a04930f8bcc61e509dfde8414a60ce3d
09ubberboy90 <2330834a@student.gla.ac.uk>
2024-09-23 17:26:47 +0100

    Remove collision visualisation

--- general_world.py
+++ general_world.py
@@ -129,7 +129,7 @@ class TeleopWorld(Node):
             if self._robot_2 is not None:
                 self._robot_2.motion_policy.add_obstacle(self.cortex_table)
 
-        self._robot.motion_policy.visualize_collision_spheres()
+        # self._robot.motion_policy.visualize_collision_spheres()
         # self._robot_2.motion_policy.visualize_collision_spheres()
 
     def create_action_graph(self):
